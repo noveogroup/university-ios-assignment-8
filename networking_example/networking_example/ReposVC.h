ea82ef8c8d97930357d1e90c61837f628590af37
Alexandr Ovchinnikov <Alintorius@gmail.com>
2015-03-22 19:39:44 +0600

    Add reposVC with tableView

---
+++ networking_example/networking_example/ReposVC.h
@@ -0,0 +1,9 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface ReposVC : UIViewController
+
+@property (strong, nonatomic) NSArray *repositories;
+
+@end
---
+++ networking_example/networking_example/ReposVC.m
@@ -0,0 +1,17 @@
+
+#import "ReposVC.h"
+
+@interface ReposVC () <UITableViewDataSource, UITableViewDelegate>
+
+@end
+
+@implementation ReposVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
